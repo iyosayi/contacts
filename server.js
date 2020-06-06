72400c2b4bb03cf1bf02401bf6e28f5b734f170e
Iyosayi <Iyosa14@gmail.com>
2020-06-06 08:07:20 +0100

    Express react app

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Connect DB
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the contact keeper API" })
+);
+
+//Define routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server started"));

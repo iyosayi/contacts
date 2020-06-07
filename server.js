cc22ce4f75c5cfb3b52d914094cdb35236915ac9
Iyosayi <Iyosa14@gmail.com>
2020-06-07 01:31:07 +0100

    added production config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,10 +10,15 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the contact keeper API" })
-);
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set a static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
